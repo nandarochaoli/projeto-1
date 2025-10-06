6603549207b841acea86bb7c608eb494222394ea
nandarochaoli <maferochaoliveira@gmail.com>
2025-10-06 15:34:06 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,4 +4,6 @@ st.write("Alô mundo")
 
 import streamlit as st
 
-st.image("imagens/paisagem.jpg", caption="Paisagem", use_container_width=True)
+st.image("projeto 2/imagens/paisagem.jpg", caption="Paisagem", use_container_width=True)
+
+streamlit run app.py
