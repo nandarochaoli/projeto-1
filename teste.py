7f466f47b40c651bd2fb9e9f07189115abe92d3c
nandarochaoli <maferochaoliveira@gmail.com>
2025-10-07 18:37:15 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.write("Alô mundo")
-st.title("Título")
-st.header("Subtítulo")
-st.write("Texto")
+st.title("Alô mundo!🪩")
+st.header("Bem-vindo ao site mais novo da Nanda!💖")
+st.write("Só preencha abaixo com algumas informaçõezinhas paa conhecê-lo melhor.😁")
+
+nome = st.text_input("Qual é o seu nome?")
+if nome:
+    st.write("Olá, {nome}! Seja bem-vindo(a) ao meu app!😊")
